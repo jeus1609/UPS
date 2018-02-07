8bbebfffcf6e06aa1993ad48f6c72cb86049d074
Johan <jeus1609@gmail.com>
2018-02-07 11:30:06 -0500

    Cambiando enies

--- application/models/Model_SeguimientoCertificado.php
+++ application/models/Model_SeguimientoCertificado.php
@@ -230,7 +230,7 @@ class Model_SeguimientoCertificado extends CI_Model
 		$data = array(
 				'ano_eje' => $ano_eje,
 				'finalidad' => $finalidad,
-				'nombre' => $nombre,
+				'nombre' => special_convert($nombre),
 				'estado' => $estado,
 				'ambito' => $ambito,
 				'es_presupuestal' => $es_presupuestal,
@@ -309,4 +309,46 @@ class Model_SeguimientoCertificado extends CI_Model
 				COMMIT TRAN T1"); 
 		return true;
 	 }
+
+	public function special_convert($cadena)
+    {
+
+        //Codificamos la cadena en formato utf8 en caso de que nos de errores
+        $cadena = utf8_encode($cadena);
+
+        //Ahora reemplazamos las letras
+        $cadena = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $cadena
+        );
+
+        $cadena = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $cadena);
+
+        $cadena = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $cadena);
+
+        $cadena = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $cadena);
+
+        $cadena = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $cadena);
+
+        $cadena = str_replace(
+            array('ñ', 'Ñ', 'ç', 'Ç'),
+            array('n', 'N', 'c', 'C'),
+            $cadena
+        );
+
+        return $cadena;
+    }
 }
